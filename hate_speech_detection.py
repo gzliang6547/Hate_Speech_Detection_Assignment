79e3bb7d2113bb99efb4351f68ffd55826f8f40c
gzliang6547 <141334045+gzliang6547@users.noreply.github.com>
2024-07-31 22:26:56 +0800

    Update hate_speech_detection.py

--- hate_speech_detection.py
+++ hate_speech_detection.py
@@ -18,20 +18,19 @@ def main():
     
     if st.button('Detect'):
         if input_name.strip() != '':
-	    
-	    test_input = tfidf_vector.transform(input_text)
+		test_input = tfidf_vector.transform(input_text)
 
-	    res=naive_bayes_classifier.predict(test_input)[0]
+	    	res=naive_bayes_classifier.predict(test_input)[0]
 
-	    if res==1:
-	         detection="Hateful"
-	    elif res==0:
-	         detection="Non-Hateful"
+	    	if res==1:
+	        	detection="Hateful"
+	    	elif res==0:
+	        	 detection="Non-Hateful"
             
             # Display prediction
-            st.success(f'The sentence {input_text} is a {detection}')
+            	st.success(f'The sentence {input_text} is a {detection}')
         else:
-            st.warning('Please enter some words.')
+            	st.warning('Please enter some words.')
 
 if __name__ == '__main__':
     main()
